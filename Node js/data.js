f3db0b5fc16a74edc3175a517664f00a10644170
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-06 23:45:43 +0530

    Files

--- Node js/data.js
+++ Node js/data.js
@@ -1,3 +1,12 @@
 //Here we will print the result we gathered from the Data folder.
 let info = require("../Fruits"); //A whole directory name
 console.log(info);
+
+console.log(info[0].name)
+console.log(info[0])
+
+console.log(info[1].name)
+console.log(info[1])
+
+console.log(info[2].name)
+console.log(info[2])
