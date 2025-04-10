c6f7913d9473cebf587e8491a54ba29aa7370e3d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-10 19:34:08 +0530

    Import Modules

--- Import Modules/script.js
+++ Import Modules/script.js
@@ -4,3 +4,10 @@ import {sum, mult, PI} from "./math.js";
 console.log(sum(2,4));
 console.log(mult(2,4));
 console.log("Value of PI", PI);
+
+//Require vs Import
+// :1 We can't selectively load only the pieces we need with require but with import we can selectively load only the pieces we need,
+// which can save memory.
+
+// :2 The import statement is only available in ES6 and later versions of JavaScript.
+// Loading is synchornous for "require" but can be asynchronous for "import".
